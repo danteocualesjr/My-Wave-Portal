6cb4e22ca0264efe1a87baaaba1ec3d5586e5bca
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-06 13:06:31 +0800

    Created my very first smart contract! Also created a script (run.js) to run it

---
+++ scripts/run.js
@@ -0,0 +1,21 @@
+// This is a script to run the smart contract
+
+const main = async () => {
+    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+    console.log('Contract deployed to:', waveContract.address);
+}; 
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    }   catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
+
