c6de3f3f9f0b927884373566b349ef5257225a2a
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-07 10:42:57 +0800

    Updated run.js to call the functions created in WavePortal.sol for storing data (waves)

--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,22 @@
 // This is a script to run the smart contract
 
 const main = async () => {
+    // Updating run.js to call the functions I created for storing data (waves)
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
     console.log('Contract deployed to:', waveContract.address);
+    console.log('Contract deployed by:', owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn. wait();
+
+    waveCount = await waveContract.getTotalWaves();
 }; 
 
 const runMain = async () => {
