d748607e5c0c5b7b859569b5e6f549b34c4f9b83
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-09 10:29:52 +0800

    Removing old Alchemy URL and private key

--- scripts/run.js
+++ scripts/run.js
@@ -17,6 +17,11 @@ const main = async () => {
     await waveTxn. wait();
 
     waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 }; 
 
 const runMain = async () => {
