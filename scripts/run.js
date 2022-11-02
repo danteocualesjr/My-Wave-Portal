3c2f731c817e71fa81875cc72182cffa9bea714c
Dante O. Cuales <danteocualesjr@gmail.com>
2022-11-02 08:48:05 +0800

    Refactored code

--- scripts/run.js
+++ scripts/run.js
@@ -10,7 +10,6 @@ const main = async () => {
     });
 
     await waveContract.deployed();   
-     
     console.log('Contract deployed addy:', waveContract.address);
     
     // console.log('Contract deployed by:', owner.address);
