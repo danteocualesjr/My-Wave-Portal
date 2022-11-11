b2ee1a6c2897f197bc7e42d5a8be54f747cb37ef
Dante O. Cuales <danteocualesjr@gmail.com>
2022-11-12 07:54:55 +0800

    Refactored code

--- scripts/run.js
+++ scripts/run.js
@@ -80,12 +80,13 @@ const main = async () => {
 const runMain = async () => {
 
     try {
-        
+
         await main();
         process.exit(0);
     }   catch (error) {
         console.log(error);        
-        process.exit(1);                
+        process.exit(1);  
+                      
     } 
 
 };
