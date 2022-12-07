5a83669e003f1c0a5a570503c7ddfe54d107919c
Dante O. Cuales <danteocualesjr@gmail.com>
2022-12-07 09:16:27 +0800

    Refactored code

--- scripts/run.js
+++ scripts/run.js
@@ -48,6 +48,7 @@ const main = async () => {
     await waveTxn2.wait();
     
     contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    
     console.log(
         'Contract balance:',
         hre.ethers.utils.formatEther(contractBalance)    
