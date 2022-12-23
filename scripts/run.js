1fbbeb1ac5e25dfff829af46cd1c09f994e98120
Dante O. Cuales <danteocualesjr@gmail.com>
2022-12-24 04:10:35 +0800

    Refactored code

--- scripts/run.js
+++ scripts/run.js
@@ -8,7 +8,7 @@ const main = async () => {
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');  
 
     const waveContract = await waveContractFactory.deploy({   
-                    
+
         value: hre.ethers.utils.parseEther('0.1'),  
 
     });
@@ -25,6 +25,7 @@ const main = async () => {
     */
 
     let contractBalance = await hre.ethers.provider.getBalance(  
+        
         waveContract.address 
     );  
       
