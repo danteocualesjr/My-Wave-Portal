96e5917cfc205864569de92114c6f64a666a80af
Dante O. Cuales <danteocualesjr@gmail.com>
2022-12-25 10:32:05 +0800

    Refactored code

--- scripts/run.js
+++ scripts/run.js
@@ -31,7 +31,7 @@ const main = async () => {
     );  
       
     console.log(
-        
+
         'Contract balance:', 
 
         hre.ethers.utils.formatEther(contractBalance)    
@@ -41,6 +41,7 @@ const main = async () => {
     // Sending two waves
 
     const waveTxn = await waveContract.wave('This is wave # 1');
+    
     await waveTxn.wait();
     const waveTxn2 = await waveContract.wave('This is wave # 2');   
     await waveTxn2.wait();    
