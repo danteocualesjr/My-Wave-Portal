6fffa5bad6e237b8370a0df6cb0caacf0d3ce5c2
Dante O. Cuales <danteocualesjr@gmail.com>
2023-01-08 08:07:56 +0800

    Refactored code

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,10 @@
 // This is a script to run the smart contract
 
 const main = async () => {   
-    
+
     // Updating run.js to call the functions I created for storing data (waves)
     // const [owner, randomPerson] = await hre.ethers.getSigners(); 
+    
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');  
     const waveContract = await waveContractFactory.deploy({   
         value: hre.ethers.utils.parseEther('0.1'),  
