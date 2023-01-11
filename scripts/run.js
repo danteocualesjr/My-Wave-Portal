bea106bc2bd1f534985fa673feb0996bd02193b7
Dante O. Cuales <danteocualesjr@gmail.com>
2023-01-11 08:01:54 +0800

    Refactored code

--- scripts/run.js
+++ scripts/run.js
@@ -32,6 +32,7 @@ const main = async () => {
     );  
       
     console.log(
+        
         'Contract balance:', 
         hre.ethers.utils.formatEther(contractBalance)    
                                
