ba5939f245106f59ba9282815b769a435d2808de
Dante O. Cuales <danteocualesjr@gmail.com>
2023-01-29 10:08:29 +0800

    Refactored code

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Greeter {
-    
+
     string private greeting;
 
     constructor(string memory _greeting) {
+        
         console.log("Deploying a Greeter with greeting:", _greeting);
         greeting = _greeting;
     }
