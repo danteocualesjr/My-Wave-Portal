167c1b3ffa468c6dff2c88fa8ce923beb2356941
Dante O. Cuales <danteocualesjr@gmail.com>
2023-01-30 05:12:16 +0800

    Refactored code

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -21,6 +21,7 @@ contract Greeter {
 
     function setGreeting(string memory _greeting) public {
         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+        
         greeting = _greeting;
 
     }
