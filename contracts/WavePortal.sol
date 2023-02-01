92ddfe09e6e63afd23165000e1a974b73e654d52
Dante O. Cuales <danteocualesjr@gmail.com>
2023-02-02 06:49:55 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -69,8 +69,9 @@ contract WavePortal {
                 "Trying to withdraw more money than the contract has."
 
             );
-            
+
             (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            
             require(success, "Failed to withdraw money from contract.");            
         }
 
