187a844883fe1344e72c0808a3b7a2399ad97843
Dante O. Cuales <danteocualesjr@gmail.com>
2023-02-10 08:16:40 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -33,7 +33,6 @@ contract WavePortal {
         totalWaves += 1;
         console.log('%s waved w/ message %s', msg.sender);
         waves.push(Wave(msg.sender, _message, block.timestamp));
-
         seed = (block.difficulty + block.timestamp + seed) % 100;
 
         // console.log('Random # generated: %d', seed);
