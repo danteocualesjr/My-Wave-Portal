05846992939806584500a9304ea0797dc7046a3a
Dante O. Cuales <danteocualesjr@gmail.com>
2023-02-15 08:47:18 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -52,6 +52,7 @@ contract WavePortal {
         emit NewWave(msg.sender, block.timestamp, _message);
 
     }
+    
     function getAllWaves() public view returns (Wave[] memory) {
         return waves;
     }
