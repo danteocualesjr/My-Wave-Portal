8809cff4fefe27ffc170b17822121235c5ed7d58
Dante O. Cuales <danteocualesjr@gmail.com>
2023-02-19 18:37:35 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -37,9 +37,10 @@ contract WavePortal {
 
         // console.log('Random # generated: %d', seed);
 
-        if (seed <= 50) {            
+        if (seed <= 50) {  
+                      
             console.log('%s won!', msg.sender);
-            
+
             uint256 prizeAmount = 0.0001 ether;
 
             require(
