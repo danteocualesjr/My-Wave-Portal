575955957267120a0463e2f286195b3c3bd8cb86
Dante O. Cuales <danteocualesjr@gmail.com>
2023-02-20 16:48:08 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -31,8 +31,9 @@ contract WavePortal {
 
         lastWavedAt[msg.sender] = block.timestamp;
         totalWaves += 1;
-        console.log('%s waved w/ message %s', msg.sender);
         
+        console.log('%s waved w/ message %s', msg.sender);
+
         waves.push(Wave(msg.sender, _message, block.timestamp));
         
         seed = (block.difficulty + block.timestamp + seed) % 100;
