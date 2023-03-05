729eb72ff0a94be2af204e59ac3da9bc0a8cdac6
Dante O. Cuales <danteocualesjr@gmail.com>
2023-03-05 18:58:53 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -9,7 +9,6 @@ contract WavePortal {
     event NewWave(address indexed from, uint256 timestamp, string message);
     struct Wave {
         address waver;
-        
         string message;    
 
         uint256 timestamp;
