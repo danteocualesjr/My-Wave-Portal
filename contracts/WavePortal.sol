9cdb3f9368c9bf7099b91842c2ac3d4724e4b3ec
Dante O. Cuales <danteocualesjr@gmail.com>
2023-04-27 09:36:50 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -11,8 +11,9 @@ contract WavePortal {
     uint256 private seed;   
      
     event NewWave(address indexed from, uint256 timestamp, string message);
-    
-    struct Wave {        
+
+    struct Wave {     
+           
         address waver;
         string message;    
         uint256 timestamp;
