d7842f7e697ec6ec9204bc0e2d5f134db58a7145
Dante O. Cuales <danteocualesjr@gmail.com>
2023-07-01 10:44:22 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -46,7 +46,6 @@ contract WavePortal {
             require(success, "Failed to withdraw money from contract.");                                    
         }       
         emit NewWave(msg.sender, block.timestamp, _message);
-
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
