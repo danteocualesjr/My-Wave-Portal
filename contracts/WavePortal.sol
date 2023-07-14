5796eebf75adc9421e7a1366a34e13034e10f1c6
Dante O. Cuales <danteocualesjr@gmail.com>
2023-07-14 09:37:44 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -74,7 +74,8 @@ contract WavePortal {
 
             (bool success, ) = (msg.sender).call{value: prizeAmount}("");
             
-            require(success, "Failed to withdraw money from contract.");                                    
+            require(success, "Failed to withdraw money from contract."); 
+                                               
         }       
         emit NewWave(msg.sender, block.timestamp, _message);
     }
