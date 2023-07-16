5c63dcd2c79b91d614d0325b467b29dcc81bdf79
Dante O. Cuales <danteocualesjr@gmail.com>
2023-07-16 09:07:05 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -84,6 +84,7 @@ contract WavePortal {
     
     function getAllWaves() public view returns (Wave[] memory) {
         return waves;      
+        
     }
     function getTotalWaves() public view returns (uint256) {
         
