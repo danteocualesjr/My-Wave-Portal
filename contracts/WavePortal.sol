642cd96e2317bac6c2701d9410a4604508b28968
Dante O. Cuales <danteocualesjr@gmail.com>
2023-07-17 08:38:45 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
- 
 import "hardhat/console.sol";
 
 contract WavePortal {
@@ -94,6 +93,6 @@ contract WavePortal {
         return totalWaves;       
 
     }   
-    
+
 }
 
