9e57cb111b668671044b5fcd73a9518013f6dca4
Dante O. Cuales <danteocualesjr@gmail.com>
2023-07-18 08:03:36 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
-
     uint256 private seed;   
 
     event NewWave(address indexed from, uint256 timestamp, string message);
