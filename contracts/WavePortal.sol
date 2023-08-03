086c283a289fbba8b0a5900a9822dee44bdca1b8
Dante O. Cuales <danteocualesjr@gmail.com>
2023-08-04 07:11:11 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -26,8 +26,9 @@ contract WavePortal {
     mapping (address => uint256) public lastWavedAt;
     
     constructor () payable {
-        
+
         console.log('We have been constructed!');
+        
         seed = (block.timestamp + block.difficulty) % 100;   
     }
 
