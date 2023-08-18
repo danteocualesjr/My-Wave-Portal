b8f97ebbc7590bd20525db901222dca22c4ea8c3
Dante O. Cuales <danteocualesjr@gmail.com>
2023-08-18 08:33:25 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -25,7 +25,6 @@ contract WavePortal {
 
     function wave(string memory _message) public {
         require(      
-
             lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
             
             'Wait 30 seconds'
