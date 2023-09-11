fadd778eece162db64142ad610923dc67bd6bff1
Dante O. Cuales <danteocualesjr@gmail.com>
2023-09-11 11:12:21 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,7 +6,6 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
     uint256 private seed;  
-
     event NewWave(address indexed from, uint256 timestamp, string message);
 
     struct Wave {     
