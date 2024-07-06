abc3849b1491e5168a9522668634ebeb0a87ac18
Dante O. Cuales <danteocualesjr@gmail.com>
2024-07-06 10:23:21 +0800

    Refactored code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -40,7 +40,6 @@ contract WavePortal {
             uint256 prizeAmount = 0.0001 ether;
             require(
                 prizeAmount <= address(this).balance,
-
                 "Trying to withdraw more money than the contract has."  
 
             );
